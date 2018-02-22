33255bed744f264dbf19992512986a7058b9720b
Miljan <miljan@aleksic.es>
2018-02-22 14:26:33 +0800

    fix when no options provided (#1)
    * fix if no options provided
    
    * tweaks

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const vmark = require('vmark')
 const loaderUtils = require('loader-utils')
 
 module.exports = function (source) {
-  const options = loaderUtils.getOptions(this)
+  const options = loaderUtils.getOptions(this) || {}
   return vmark(source, options).component
 }

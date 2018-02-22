198b6be2ea4f08b222cc13f9d0910a96cc79a750
EGOIST <0x142857@gmail.com>
2018-02-22 14:26:17 +0800

    tweaks

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const vmark = require('vmark')
 const loaderUtils = require('loader-utils')
 
 module.exports = function (source) {
-  const options = loaderUtils.getOptions(this)
-  return vmark(source, options || {}).component
+  const options = loaderUtils.getOptions(this) || {}
+  return vmark(source, options).component
 }
